01ae0542ad4b05cc7f59e5ca1f857019782aab2c
Petrik Diak <diak@petrik.hu>
2025-11-25 13:29:00 +0100

    kesz done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,42 +1,35 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Pizza
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-		private void buttonOsszegzes_Click(object sender, EventArgs e)
+
+		private void buttonOsszegzes_Click(object sender, RoutedEventArgs e)
 		{
 			string teszta = listBoxTesztak.SelectedItem?.ToString() ?? "(Nem választottál tésztát)";
-			string meret = comboBoxMeret.SelectedItem?.ToString() ?? "(Nem választottál méretet)";
+			string meret = (comboBoxMeret.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "(Nem választottál méretet)";
 
 			List<string> feltetek = new List<string>();
-			if (checkBoxSajt.Checked) feltetek.Add("Sajt");
-			if (checkBoxSonka.Checked) feltetek.Add("Sonka");
-			if (checkBoxGomba.Checked) feltetek.Add("Gomba");
-			if (checkBoxOlivabogyo.Checked) feltetek.Add("Olívabogyó");
-			if (checkBoxKukorica.Checked) feltetek.Add("Kukorica");
+			if (checkBoxSajt.IsChecked == true) feltetek.Add("Sajt");
+			if (checkBoxSonka.IsChecked == true) feltetek.Add("Sonka");
+			if (checkBoxGomba.IsChecked == true) feltetek.Add("Gomba");
+			if (checkBoxOlivabogyo.IsChecked == true) feltetek.Add("Olívabogyó");
+			if (checkBoxKukorica.IsChecked == true) feltetek.Add("Kukorica");
 
 			string feltetSzoveg = feltetek.Count > 0 ? string.Join(", ", feltetek) : "(Nincs plusz feltét)";
-			string atvetel = radioButtonHazhoz.Checked ? "Házhozszállítás" : (radioButtonSzemelyes.Checked ? "Személyes átvétel" : "(Nincs megadva)");
+			string atvetel = radioButtonHazhoz.IsChecked == true ? "Házhozszállítás"
+				: radioButtonSzemelyes.IsChecked == true ? "Személyes átvétel"
+				: "(Nincs megadva)";
 
 			string osszegzes = $"Tészta: {teszta}\nMéret: {meret}\nFeltétek: {feltetSzoveg}\nÁtvételi mód: {atvetel}";
-			labelOsszegzes.Text = osszegzes;
+			labelOsszegzes.Content = osszegzes;
 		}
 	}
 }
