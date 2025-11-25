addd1d37cd892551b73cfad5a568f8327c5ff820
Petrik Diak <diak@petrik.hu>
2025-11-25 13:11:32 +0100

    checkbox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,23 @@ namespace Pizza
         {
             InitializeComponent();
         }
-    }
+		private void buttonOsszegzes_Click(object sender, EventArgs e)
+		{
+			string teszta = listBoxTesztak.SelectedItem?.ToString() ?? "(Nem választottál tésztát)";
+			string meret = comboBoxMeret.SelectedItem?.ToString() ?? "(Nem választottál méretet)";
+
+			List<string> feltetek = new List<string>();
+			if (checkBoxSajt.Checked) feltetek.Add("Sajt");
+			if (checkBoxSonka.Checked) feltetek.Add("Sonka");
+			if (checkBoxGomba.Checked) feltetek.Add("Gomba");
+			if (checkBoxOlivabogyo.Checked) feltetek.Add("Olívabogyó");
+			if (checkBoxKukorica.Checked) feltetek.Add("Kukorica");
+
+			string feltetSzoveg = feltetek.Count > 0 ? string.Join(", ", feltetek) : "(Nincs plusz feltét)";
+			string atvetel = radioButtonHazhoz.Checked ? "Házhozszállítás" : (radioButtonSzemelyes.Checked ? "Személyes átvétel" : "(Nincs megadva)");
+
+			string osszegzes = $"Tészta: {teszta}\nMéret: {meret}\nFeltétek: {feltetSzoveg}\nÁtvételi mód: {atvetel}";
+			labelOsszegzes.Text = osszegzes;
+		}
+	}
 }
